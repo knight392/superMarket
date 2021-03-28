1f07a27063a92b927a5896063ab5c2ef2a14b7d4
knight392 <knight_yya@163.com>
2021-03-28 21:25:48 +0800

    add feature-view

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,6 +3,7 @@
     <div class="home-nav"><nav-bar><div slot="center">购物街</div></nav-bar></div>
     <home-swiper :banner="banner"/>
     <recommend-view :recommends="recommends"/>
+    <feature-view/>
   </div>
 </template>
 
@@ -10,12 +11,15 @@
 import navBar from 'components/common/navbar/NavBar'
 import HomeSwiper from './childCopms/HomeSwiper'
 import RecommendView from './childCopms/recommendView'
+import FeatureView from './childCopms/featureView'
 import {getHomeMultiData} from 'network/home'
+
 export default {
   components: {
     navBar,
     HomeSwiper,
-    RecommendView
+    RecommendView,
+    FeatureView
   },
   data () {
     return {
@@ -33,7 +37,15 @@ export default {
 </script>
 
 <style scoped>
+  #home{
+    padding-top:44px;
+  }
   .home-nav{
+    position:fixed;
+    top:0;
+    left:0;
+    right:0;
+    z-index:100;
     background-color: var(--color-tint);
     color:#fff;
   }
