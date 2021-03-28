3897fe76957bd7b43bf49e4e721a5ed34874d3ae
knight392 <knight_yya@163.com>
2021-03-28 20:41:43 +0800

    add recommend-view

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,17 +2,20 @@
   <div id='home'>
     <div class="home-nav"><nav-bar><div slot="center">购物街</div></nav-bar></div>
     <home-swiper :banner="banner"/>
+    <recommend-view :recommends="recommends"/>
   </div>
 </template>
 
 <script>
 import navBar from 'components/common/navbar/NavBar'
 import HomeSwiper from './childCopms/HomeSwiper'
+import RecommendView from './childCopms/recommendView'
 import {getHomeMultiData} from 'network/home'
 export default {
   components: {
     navBar,
-    HomeSwiper
+    HomeSwiper,
+    RecommendView
   },
   data () {
     return {
---
+++ src/views/home/childCopms/recommendView.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="recommend">
+    <div v-for="(item, index) in recommends" :key="index" class="recommend-item">
+      <img :src="item.image"/>
+      <div>{{item.title}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ props: {
+   recommends: {
+     type: Array,
+     default () {
+       return []
+     }
+   }
+ }
+}
+</script>
+
+<style scoped>
+  .recommend {
+    display: flex;
+    padding:10px 0 20px;
+    border-bottom: 8px solid #eee;
+    font-size:12px;
+    color:var(--color-text)
+  }
+  .recommend-item {
+    /* 宽度平均分配 */
+    flex:1;
+    align-content:center;
+    text-align: center;
+  }
+  .recommend-item img {
+    width:65px;
+    height:65px;
+    margin-bottom:8px;
+  }
+</style>
